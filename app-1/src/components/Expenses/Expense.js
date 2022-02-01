01fb1342bba4d1903077d42c93430ef98cc829c0
pankaj kumar <raipankaj150@gmail.com>
2022-02-01 22:15:57 +0530

    Update Expense.js

--- app-1/src/components/Expenses/Expense.js
+++ app-1/src/components/Expenses/Expense.js
@@ -2,9 +2,16 @@ import ExpenseItem from "./ExpenseItem";
 
 const Expense = ({ expenses }) => {
 
+    let expensesContent = <p>No Expenses Found</p>;
+
+    if (expenses.length > 0) {
+        expensesContent = expenses.map(expense => <ExpenseItem date={expense.date}
+            title={expense.title} amount={expense.amount} />)
+    };
+
     return (
         <div>
-            {expenses.map(expense => <ExpenseItem date={expense.date} title={expense.title} amount={expense.amount} />)}
+            {expensesContent}
         </div>
     );
 }
