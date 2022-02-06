e994eda5c256b556de7e35e8377910b0234837b5
pankaj kumar <raipankaj150@gmail.com>
2022-02-06 11:33:24 +0530

    styling complete

--- app-2/src/components/UI/Button/Button.js
+++ app-2/src/components/UI/Button/Button.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import './Button.css';
+// import styled from 'styled-components'
+import styles from './Button.module.css';
+
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
