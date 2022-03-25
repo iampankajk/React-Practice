6c6ffd4ae727586e59e06d3a26b905bc90b0fc55
pankaj kumar <raipankaj150@gmail.com>
2022-03-25 21:42:12 +0530

    http

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://react-practise-b6d2c-default-rtdb.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
