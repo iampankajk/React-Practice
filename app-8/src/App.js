a89e28c078e3a678f62f88a6e47c1ff20112f530
pankaj kumar <raipankaj150@gmail.com>
2022-03-26 19:22:23 +0530

    completed

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState} from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -7,20 +7,21 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTask = taskObj=>{
-    const loadedTasks = [];
-
-      for (const taskKey in taskObj) {
-        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-  }
- const {isLoading,error,sendRequest:fetchTasks} =  useHttp({url:'https://react-practise-b6d2c-default-rtdb.firebaseio.com/tasks.json'},
-  transformTask);
+  
+ const {isLoading,error,sendRequest:fetchTasks} =  useHttp();
 
   useEffect(() => {
-    fetchTasks();
+    const transformTask =taskObj=>{
+      const loadedTasks = [];
+  
+        for (const taskKey in taskObj) {
+          loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+        }
+  
+        setTasks(loadedTasks);
+    }
+    fetchTasks({url:'https://react-practise-b6d2c-default-rtdb.firebaseio.com/tasks.json'},
+    transformTask);
   }, []);
 
   const taskAddHandler = (task) => {
--- app-8/src/hooks/use-http.js
+++ app-8/src/hooks/use-http.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
-const useHttp = (requestConfig,applyData)=>{
+const useHttp = ()=>{
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
     
   
-    const sendRequest = async (taskText) => {
+    const sendRequest = useCallback(async (requestConfig,applyData) => {
       setIsLoading(true);
       setError(null);
       try {
@@ -28,7 +28,7 @@ const useHttp = (requestConfig,applyData)=>{
         setError(err.message || 'Something went wrong!');
       }
       setIsLoading(false);
-    };
+    },[]);
     return {
         isLoading,
         error,
